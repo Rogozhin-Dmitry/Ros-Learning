16255e97c08fe0d3df151cac8f1b1cc298996dc6
Rogozhin-Dmitry <rogozhin-04@inbox.ru>
2024-03-28 14:36:29 +0300

    Fix pep8 with black

--- ros_learning/AdminCatalog/apps.py
+++ ros_learning/AdminCatalog/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class AdmincatalogConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'AdminCatalog'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "AdminCatalog"
