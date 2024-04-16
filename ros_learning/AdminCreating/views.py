fc8ef3a530ad602d702c944bd77b91aac9a09e77
Rogozhin-Dmitry <rogozhin-04@inbox.ru>
2024-04-16 09:41:07 +0300

    Hot fix of pep8 for main

--- ros_learning/AdminCreating/views.py
+++ ros_learning/AdminCreating/views.py
@@ -5,6 +5,7 @@ from django.views import View
 
 # Create your views here.
 
+
 class CreatingTasks(View):
     def get(self, request, *args, **kwargs):
         return HttpResponse("<h1>CreatingTasks</h1>")
