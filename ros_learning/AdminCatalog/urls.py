2a20dcdebcdd26f9738029d91be6f1afd5d16700
Rogozhin-Dmitry <rogozhin-04@inbox.ru>
2024-04-17 20:06:09 +0300

    Fixed post requests

--- ros_learning/AdminCatalog/urls.py
+++ ros_learning/AdminCatalog/urls.py
@@ -4,11 +4,11 @@ from .views import Tasks
 from .views import Lessons
 from .views import CreateCourse
 
-app_name = 'courses'
+app_name = "courses"
 
 urlpatterns = [
     path("admin-catalog/courses/", Courses.as_view(), name="courses"),
-    path("admin-catalog/courses/<int:pk>/lessons", Lessons.as_view(), name="lessons"),
-    path("admin-catalog/courses/createCourse", CreateCourse.as_view(), name="lessons"),
-    # path("admin-catalog/courses/<int:pk>/lessons/<int:pk>/tasks", Tasks.as_view(), name="tasks"),
+    path("admin-catalog/courses/<int:courses_pk>/lessons", Lessons.as_view(), name="lessons"),
+    path("admin-catalog/courses/createCourse", CreateCourse.as_view(), name="create_course"),
+    path("admin-catalog/courses/<int:courses_pk>/lessons/<int:lesson_pk>/tasks", Tasks.as_view(), name="tasks"),
 ]
--- ros_learning/AdminCatalog/views.py
+++ ros_learning/AdminCatalog/views.py
@@ -1,41 +1,39 @@
-from django.http import HttpResponse
+import json
+
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
+
 from .control import Controll
 
 
 class Courses(View):
     def get(self, request, *args, **kwargs):
-        return render(request, "courses/courses.html",
-                      {"courses": Controll.get_user_courses(request.user.id)})
+        return render(request, "courses/courses.html", {"courses": Controll.get_user_courses(request.user.id)})
 
     def post(self, request, *args, **kwargs):
-        return render(request, "courses/courses.html",
-                      {"courses": Controll.get_user_courses(request.user.id)})
+        return render(request, "courses/courses.html", {"courses": Controll.get_user_courses(request.user.id)})
 
     def update(self, request, *args, **kwargs):
-        return render(request, "courses/courses.html",
-                      {"courses": Controll.get_user_courses(request.user.id)})
+        return render(request, "courses/courses.html", {"courses": Controll.get_user_courses(request.user.id)})
 
     def delete(self, request, *args, **kwargs):
-        return render(request, "courses/courses.html",
-                      {"courses": Controll.get_user_courses(request.user.id)})
+        return render(request, "courses/courses.html", {"courses": Controll.get_user_courses(request.user.id)})
 
 
 class CreateCourse(View):
     def post(self, request, *args, **kwargs):
-        Controll.create_course()
-        return render(request, "courses/courses.html",
-                      {"courses": Controll.get_user_courses(request.user.id)})
+        data = json.loads(request.body.decode("utf-8"))
+        print(data)
+        # Controll.create_course()
+        return JsonResponse({"status": "ok"})
 
 
 class Lessons(View):
     def get(self, request, *args, **kwargs):
-        return render(request, "lessons/lessons.html",
-                      {"lessons": Controll.get_user_lessons(request.user.id)})
+        return render(request, "lessons/lessons.html", {"lessons": Controll.get_courses_lessons(kwargs["courses_pk"])})
 
 
 class Tasks(View):
     def get(self, request, *args, **kwargs):
-        return render(request, "tasks/tasks.html",
-                      {"tasks": Controll.get_user_tasks(request.user.id)})
+        return render(request, "tasks/tasks.html", {"tasks": Controll.get_lessons_tasks(kwargs["courses_pk"], kwargs["lesson_pk"])})
